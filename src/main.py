97da0ac8be0d6aafdfa3be504221253a0505d2f0
ezegonmac <ezegonmac01@gmail.com>
2022-12-13 18:33:41 +0100

    Generate density variation plot

--- src/main.py
+++ src/main.py
@@ -4,13 +4,20 @@ from statistics.densities import *
 
 if __name__ == '__main__':
 
-    # create_dataset_density_evolution_plot(DATASET1, show=True)
+    # generate_dataset1()
+    # generate_dataset2()
+    # generate_dataset3()
+    # generate_dataset4()
+    # generate_dataset5()
+    
     # create_dataset_density_evolution_plot(DATASET1)
     # create_dataset_density_evolution_plot(DATASET2)
     # create_dataset_density_evolution_plot(DATASET3)
-    
-    generate_dataset4()
-    create_dataset_density_evolution_plot(DATASET4)
-    generate_dataset5()
-    create_dataset_density_evolution_plot(DATASET5)
-    
+    # create_dataset_density_evolution_plot(DATASET4)
+    # create_dataset_density_evolution_plot(DATASET5)
+
+    create_dataset_density_variation_plot(DATASET1)
+    create_dataset_density_variation_plot(DATASET2)
+    create_dataset_density_variation_plot(DATASET3)
+    create_dataset_density_variation_plot(DATASET4)
+    create_dataset_density_variation_plot(DATASET5)
--- src/statistics/densities.py
+++ src/statistics/densities.py
@@ -6,6 +6,8 @@ from matplotlib.ticker import MaxNLocator
 
 DENSITIES_FIGURES_FOLDER = f'{FIGURES_FOLDER}/statistics/densities'
 
+# density evolution
+
 def create_dataset_density_evolution_plot(dataset, show=False, title="Density evolution"):
     
     dataset_file = f'{DATA_FOLDER}/{dataset}/dataset.csv'
@@ -14,7 +16,7 @@ def create_dataset_density_evolution_plot(dataset, show=False, title="Density ev
     
     plot_density_evolutions(density_evolutions, title)
     
-    plt.show() if show else plt.savefig(f'{DENSITIES_FIGURES_FOLDER}/{dataset}.png', dpi=300)
+    plt.show() if show else plt.savefig(f'{DENSITIES_FIGURES_FOLDER}/density_evolution_{dataset}.png', dpi=300)
     plt.close()
     
 
@@ -25,6 +27,43 @@ def plot_density_evolutions(density_evolutions, title):
         plt.plot(densities, alpha=0.5)
     
     ax.set(ylim=(0, 1.1), xlabel='Iterations', ylabel='Density', title=title)
+    # remove right and top spines
+    ax.spines.right.set_visible(False)
+    ax.spines.top.set_visible(False)
+    # integer x axis
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
+    # better quality figures
+
+# density variation
+
+def create_dataset_density_variation_plot(dataset, show=False, title="Density variations"):
+    
+    dataset_file = f'{DATA_FOLDER}/{dataset}/dataset.csv'
+    df = pd.read_csv(dataset_file)
+    density_evolutions = df['density_evolution'].apply(literal_eval).values
+    
+    plot_density_variations(density_evolutions, title)
+    
+    plt.show() if show else plt.savefig(f'{DENSITIES_FIGURES_FOLDER}/density_variation_{dataset}.png', dpi=300)
+    plt.close()
+    
+
+def plot_density_variations(density_evolutions, title):
+    
+    density_variations = []
+    for density_evolution in density_evolutions:
+        variations = []
+        for i in range(len(density_evolution)-1):
+            variation = density_evolution[i] - density_evolution[i-1]
+            variations.append(variation)
+        density_variations.append(variations)
+    
+    ax = plt.subplot(111)
+    
+    for variation in density_variations:
+        plt.plot(variation, alpha=0.5)
+    
+    ax.set(ylim=(-1.1, 1.1), xlabel='Iterations', ylabel='Density variation', title=title)
     # remove right and top spines
     ax.spines.right.set_visible(False)
     ax.spines.top.set_visible(False)
