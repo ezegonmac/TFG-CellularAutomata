38b898e38fbf15cfc5d0b723ac0133bee09809dd
ezegonmac <ezegonmac01@gmail.com>
2022-12-13 19:38:55 +0100

    Generate density evolution plot per lt

--- src/statistics/densities.py
+++ src/statistics/densities.py
@@ -3,6 +3,8 @@ import pandas as pd
 from ast import literal_eval
 import matplotlib.pyplot as plt
 from matplotlib.ticker import MaxNLocator
+from matplotlib import cm
+import numpy as np
 
 DENSITIES_FIGURES_FOLDER = f'{FIGURES_FOLDER}/statistics/densities'
 
@@ -32,7 +34,6 @@ def plot_density_evolutions(density_evolutions, title):
     ax.spines.top.set_visible(False)
     # integer x axis
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-    # better quality figures
 
 # density variation
 
@@ -69,4 +70,41 @@ def plot_density_variations(density_evolutions, title):
     ax.spines.top.set_visible(False)
     # integer x axis
     ax.xaxis.set_major_locator(MaxNLocator(integer=True))
-    # better quality figures
+
+# density evolution per life threshold
+
+def create_dataset_density_evolution_per_lt_plot(dataset, show=False, title="Density evolution per life threshold"):
+    
+    dataset_file = f'{DATA_FOLDER}/{dataset}/dataset.csv'
+    df = pd.read_csv(dataset_file)
+    lts = df['life_threshold'].unique().tolist()
+    
+    density_evolutions_by_lt = {}
+    for lt in lts:
+        density_evolutions = df[df['life_threshold'] == lt]['density_evolution'].apply(literal_eval).values
+        density_evolutions_by_lt[lt] = density_evolutions
+    
+    plot_density_evolutions_per_lt(density_evolutions_by_lt, lts, title)
+    
+    plt.show() if show else plt.savefig(f'{DENSITIES_FIGURES_FOLDER}/density_evolution_lt_{dataset}.png', dpi=300)
+    plt.close()
+    
+
+def plot_density_evolutions_per_lt(density_evolutions_by_lt, lts, title):
+    ax = plt.subplot(111)
+    
+    cmap = cm.get_cmap('jet')
+    colors = cmap(np.linspace(0, 1, len(lts)))
+    colors_by_lt = {lts[i]: colors[i] for i in range(len(lts))}
+    for lt in lts:
+        density_evolutions = density_evolutions_by_lt[lt]
+        color = colors_by_lt[lt]
+        for densities in density_evolutions:
+            plt.plot(densities, c=color, alpha=0.5)
+    
+    ax.set(ylim=(0, 1.1), xlabel='Iterations', ylabel='Density', title=title)
+    # remove right and top spines
+    ax.spines.right.set_visible(False)
+    ax.spines.top.set_visible(False)
+    # integer x axis
+    ax.xaxis.set_major_locator(MaxNLocator(integer=True))
