f6b5055eec2e714ad037e0b586d82e36d57190e8
ezegonmac <ezegonmac01@gmail.com>
2023-01-13 20:41:34 +0100

    Generate learning test figures for dataset9

--- src/learning/BTST_density.py
+++ src/learning/BTST_density.py
@@ -46,6 +46,26 @@ def evaluate_dataset3():
     generate_score_evolution_comparison_plot(dataset, metric="R2", y_min=0.96, y_max=1, suffix='scaled')
 
 
+def evaluate_dataset9():
+    dataset = DATASET9_DENSITY
+    evaluate_dataset(dataset)
+    
+    # FIGURES
+    # score model comparison plots
+    generate_scores_model_comparison_plot(dataset, metric='MSE')
+    generate_scores_model_comparison_plot(dataset, metric='R2')
+    # generate_scores_model_comparison_plot(dataset, metric='MSE', y_min=0, y_max=0.02, suffix='scaled')
+    # generate_scores_model_comparison_plot(dataset, metric='R2', y_min=0.95, y_max=1.0, suffix='scaled')
+    
+    # score evolution plots
+    generate_score_evolution_plots(dataset)
+    
+    generate_score_evolution_comparison_plots(dataset)
+
+    # generate_score_evolution_comparison_plot(dataset, metric="MSE", y_max=0.01, suffix='scaled')
+    # generate_score_evolution_comparison_plot(dataset, metric="R2", y_min=0.96, y_max=1, suffix='scaled')
+
+
 def evaluate_dataset(dataset):
     knn_model = KNeighborsRegressor(n_neighbors=5)
     dtree_model = DecisionTreeRegressor(random_state=SKLEARN_RANDOM_SEED)
--- src/main_learning.py
+++ src/main_learning.py
@@ -6,6 +6,7 @@ from learning.evaluation import *
 if __name__ == '__main__':
     
     evaluate_dataset3()
-    # evaluate_dataset8()
+    evaluate_dataset8()
+    evaluate_dataset9()
     
     # evaluate_nn_model_ds3_1()
