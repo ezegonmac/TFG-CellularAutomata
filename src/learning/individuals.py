7502babce8a3402cb6e9a804bee9225c8939bd06
ezegonmac <ezegonmac01@gmail.com>
2023-01-18 13:00:07 +0100

    Refactor individuals functions. Merge scaled and non scaled versions

--- src/learning/individuals.py
+++ src/learning/individuals.py
@@ -9,112 +9,70 @@ from learning.models import *
 
 
 def generate_dataset3_individuals_plots():
-    generate_individuals_real_vs_predicted_plot_not_scaled(
+    generate_individuals_real_vs_predicted_plots(
         DATASET3_DENSITY, 
         NEURAL_NETWORK, 
-        num_individuals=50,
+        num_individuals=5,
+        scaled=False,
         suffix=''
         )
 
 
 def generate_dataset8_individuals_plots():
-    generate_individuals_real_vs_predicted_plot_not_scaled(
+    generate_individuals_real_vs_predicted_plots(
         DATASET8_DENSITY, 
         NEURAL_NETWORK, 
         num_individuals=10,
+        scaled=False,
         suffix=''
         )
 
 
 def generate_dataset11_individuals_plots():
-    generate_individuals_real_vs_predicted_plot_scaled(
+    generate_individuals_real_vs_predicted_plots(
         DATASET11_DENSITY, 
         NEURAL_NETWORK, 
         num_individuals=1, 
+        scaled=True,
         suffix=''
         )
 
 
 def generate_dataset12_individuals_plots():
-    generate_individuals_real_vs_predicted_plot_scaled(
+    generate_individuals_real_vs_predicted_plots(
         DATASET12_DENSITY, 
         NEURAL_NETWORK, 
         num_individuals=10, 
+        scaled=True,
         suffix=''
         )
 
 
-def generate_individuals_real_vs_predicted_plot_not_scaled(dataset, model_name, num_individuals, iterations=10, suffix=None, show=False):
-
-    split = get_dataset_density_train_test_split(dataset, scaled=False)
+def generate_individuals_real_vs_predicted_plots(dataset, model_name, num_individuals, iterations=10, scaled=False, suffix=None, show=False):
+    split = get_dataset_density_train_test_split(dataset, scaled=scaled)
     X, y, X_train, X_test, y_train, y_test = split
     
     model = load_model_from_file(dataset, model_name)
-
     print(f'Model: {model}')
     
     np.random.seed(NP_RANDOM_SEED)
     random_idx = np.random.choice(y_test.shape[0], num_individuals-1, replace=False)
     for i in random_idx:
-        generate_individual_real_vs_predicted_plot_not_scaled(
+        generate_individual_real_vs_predicted_plot(
             dataset, 
             model_name, 
             individual=i, 
             split=split,
             model=model,
             iterations=iterations,
+            scaled=scaled,
             suffix=f'random_{i}'
             )
 
 
-def generate_individual_real_vs_predicted_plot_not_scaled(dataset, model_name, individual, split, model, iterations=10, suffix=None, show=False):
+def generate_individual_real_vs_predicted_plot(dataset, model_name, individual, split, model, iterations=10, scaled=False, suffix=None, show=False):
     X, y, X_train, X_test, y_train, y_test = split
-
-    individual_real = y_test.iloc[individual]
-    individual_pred = model.predict(X_test.iloc[individual].values.reshape(1, -1))
-
-    densities_real = individual_real.values
-    densities_pred = individual_pred[0]
-
-    colors = plt.cm.jet(np.linspace(0,1,8))
-    plt.plot(densities_real, alpha=1, color=colors[2], marker='o', linestyle='--')
-    plt.plot(densities_pred, alpha=1, color=colors[6], marker='.', linestyle=':')
-    plt.legend(['Real', 'Pred'])
-    plt.ylim((-0.1, 1.1))
-    
-    suffix = f'_{suffix}' if suffix else ''
-    test_figures_folder = get_test_figures_folder(dataset)
-    individuals_folder = f'{test_figures_folder}/individuals/{dataset}'
-    create_folder_if_not_exists(individuals_folder)
-    plt.show() if show else plt.savefig(f'{individuals_folder}/individual_rvsp_{dataset}_{model_name}_i{individual}_{suffix}.png', dpi=300)
-    plt.close()
-
-
-def generate_individuals_real_vs_predicted_plot_scaled(dataset, model_name, num_individuals, iterations=10, suffix=None, show=False):
-    split = get_dataset_density_train_test_split(dataset, scaled=False)
-    X, y, X_train, X_test, y_train, y_test = split
-    
-    model = load_model_from_file(dataset, model_name)
-
-    print(f'Model: {model}')
-    
-    np.random.seed(NP_RANDOM_SEED)
-    random_idx = np.random.choice(y_test.shape[0], num_individuals-1, replace=False)
-    for i in random_idx:
-        generate_individual_real_vs_predicted_plot_scaled(
-            dataset, 
-            model_name, 
-            individual=i, 
-            split=split,
-            model=model,
-            iterations=iterations,
-            suffix=f'random_{i}'
-            )
-
-
-def generate_individual_real_vs_predicted_plot_scaled(dataset, model_name, individual, split, model, iterations=10, suffix=None, show=False):
-    X, y, X_train, X_test, y_train, y_test = split
-    _, yscaler = get_x_y_scalers(dataset)
+    _, yscaler = get_x_y_scalers(dataset) if scaled else (None, None)
 
     # predict
     individual_real = y_test.iloc[individual]
@@ -122,7 +80,7 @@ def generate_individual_real_vs_predicted_plot_scaled(dataset, model_name, indiv
 
     # inverse scale
     individual_real = individual_real
-    individual_pred = yscaler.inverse_transform(individual_pred)
+    individual_pred = yscaler.inverse_transform(individual_pred) if scaled else individual_pred
 
     # get densities
     densities_real = individual_real.values
