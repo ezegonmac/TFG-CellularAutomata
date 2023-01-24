4143ef3a2fdd26da1e474be1df884cbea4de27e9
ezegonmac <ezegonmac01@gmail.com>
2023-01-24 21:43:07 +0100

    Generate ref pages (Not working)

--- src/__init__.py
+++ src/__init__.py
@@ -5,10 +5,10 @@ Those files are the only ones that should be executed directly.
 They should be executed from the root of the project (directory containing the `src` directory).
 
 The main files are:
-- `main_datasets.py`: Main functions for generating the default datasets.
-- `main_density_datasets.py`: Main functions for generating the density datasets.
-- `main_density_plots.py`: Main functions for generating the density plots.
-- `main_doc`: Main functions for generating the documentation figures.
-- `main_learning.py`: Main functions for managing the learning process.
-- `main_states.py`: Main functions for generating states plots.
+    - `main_datasets.py`: Main functions for generating the default datasets.
+    - `main_density_datasets.py`: Main functions for generating the density datasets.
+    - `main_density_plots.py`: Main functions for generating the density plots.
+    - `main_doc`: Main functions for generating the documentation figures.
+    - `main_learning.py`: Main functions for managing the learning process.
+    - `main_states.py`: Main functions for generating states plots.
 """
