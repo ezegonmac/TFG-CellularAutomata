afdc98d94c6648b8503beecc40a0fd7fa4d43da9
ezegonmac <ezegonmac01@gmail.com>
2023-05-11 17:14:28 +0200

    Add function to generate scores tables

--- src/learning/misc/doc.py
+++ src/learning/misc/doc.py
@@ -1,25 +1,24 @@
-from learning.test.files import get_scores_by_dataset
+from learning.test.files_scores import get_scores_by_dataset
 
 
-def generate_score_by_iteration_table(dataset) -> None:
+def generate_scores_table(dataset) -> None:
     """
-    Generate a table of scores by iteration for each model and prints it in console.
+    Generate a table of scores for each model and prints it in console.
     """
     
     # Write #L and #T as { and } to avoid conflicts with latex
     # Write \ as \\ to avoid conflicts with latex
     base = '''
 \\begin{table}[!htp]
+    \\renewcommand{\\arraystretch}{2}
     \\centering
-    {\\footnotesize
-    \\begin{tabular}{|c|#Lcolumns#T|}
+    \\begin{tabular}{|c|cc|}
         \\hline
-        \\textbf{Modelo} #Llabels#T\\\\[1.1ex]
+        \\textbf{Modelo} & \\textbf{RMSE} & \\textbf{R2} \\\\[1.1ex]
         \\hline
         #Ldata#T
     \\end{tabular}
-    }
-    \\caption{Métricas de predicción por iteración para el #Ldataset#T}
+    \\caption{Métricas para el #Ldataset#T}
     \\label{tab:metrics_#Ldataset#T}
 \\end{table}
     '''
@@ -30,49 +29,138 @@ def generate_score_by_iteration_table(dataset) -> None:
     base = base.replace('#L', '{').replace('#T', '}')
     
     scores_df = get_scores_by_dataset(dataset)
+    # filter by Number of individuals
+    scores_df = scores_df[scores_df['Number of individuals'] == 1000]
+    
+    models = scores_df['Model'].unique()
+    
+    data = ''
+    rmses = []
+    r2s = []
+    for i in range(len(models)):
+        rmses = []
+        r2s = []
+        model = models[i]
+        rmse = scores_df[scores_df['Model'] == model]['RMSE mean'].values[0]
+        r2 = scores_df[scores_df['Model'] == model]['R2 mean'].values[0]
+        rmse_std = scores_df[scores_df['Model'] == model]['RMSE std'].values[0]
+        r2_std = scores_df[scores_df['Model'] == model]['R2 std'].values[0]
+
+        # mean
+        rmse_val = '{:.3e}'.format(rmse)
+        r2_val = '{:.3e}'.format(r2)
+        rmse_val = rmse_val.replace('e-0', 'e-').replace('e+0', 'e+')
+        r2_val = r2_val.replace('e-0', 'e-').replace('e+0', 'e+')
+        # std
+        rmse_std = '{:.2e}'.format(rmse_std)
+        r2_std = '{:.2e}'.format(r2_std)
+        rmse_std = rmse_std.replace('e-0', 'e-').replace('e+0', 'e+')
+        r2_std = r2_std.replace('e-0', 'e-').replace('e+0', 'e+')
+        # sum
+        rmse_val = rmse_val + '$\\pm$' + rmse_std
+        r2_val = r2_val + '$\\pm$' + r2_std
+        rmses.append(rmse_val)
+        r2s.append(r2_val)
+        
+        data += '''{model} & {rmse} & {r2} \\\\
+        \\hline
+        '''.format(model=models[i], rmse=rmses[-1], r2=r2s[-1])
+
+    iterations = len(rmses)
+    labels = ''
+    for i in range(1, iterations+1):
+        labels += '& \\textbf{{{i}}}'.format(i=i)
+
+    table = base.format(data=data, dataset=dataset, labels=labels, metric='RMSE')
+
+    print('TABLE')
+    print('\n\n')
+    
+    print(table)
+
+
+def generate_score_by_iteration_table(dataset) -> None:
+    """
+    Generate a table of RMSE and R2 scores by iteration for each model and prints it in console.
+    """
+    
+    # Write #L and #T as { and } to avoid conflicts with latex
+    # Write \ as \\ to avoid conflicts with latex
+    base = '''
+\\begin{table}[!htp]
+    \\centering
+    {\\footnotesize
+    \\setlength\\tabcolsep{7pt}
+    \\begin{tabular}{|c|#Lcolumns#T|}
+        \\hline
+        \\textbf{Modelo} #Llabels#T\\\\[1.1ex]
+        \\hline
+        #Ldata#T
+    \\end{tabular}
+    }
+    \\caption{#Lmetric#T por iteración para el #Ldataset#T}
+    \\label{tab:#Lmetric#T_#Ldataset#T}
+\\end{table}
+    '''
+    # scape characters
+    base = base.replace('{', '{{').replace('}', '}}')
+    # base = base.replace('\\', '\\\\')
+    
+    base = base.replace('#L', '{').replace('#T', '}')
+    
+    scores_df = get_scores_by_dataset(dataset)
+    # filter by Number of individuals
+    scores_df = scores_df[scores_df['Number of individuals'] == 1000]
     
     models = scores_df['Model'].unique()
     
     mse_data = ''
     r2_data = ''
-    mses = []
+    rmses = []
     r2s = []
     for i in range(len(models)):
-        mses = []
+        rmses = []
         r2s = []
         model = models[i]
-        mse = scores_df[scores_df['Model'] == model]['MSE by iteration'].values[0]
-        r2 = scores_df[scores_df['Model'] == model]['R2 by iteration'].values[0]
+        rmse = scores_df[scores_df['Model'] == model]['RMSE mean by iteration'].values[0]
+        r2 = scores_df[scores_df['Model'] == model]['R2 mean by iteration'].values[0]
         
-        for j in range(1, len(mse)+1):
-            j = str(j)
-            mses.append('%.2f' % mse[j])
-            r2s.append('%.2f' % r2[j])
+        for j in range(1, len(rmse)+1):
+
+            rmse_val = '{:.2e}'.format(rmse[j])
+            r2_val = '{:.2e}'.format(r2[j])
+            rmse_val = rmse_val.replace('e-0', 'e-').replace('e+0', 'e+')
+            r2_val = r2_val.replace('e-0', 'e-').replace('e+0', 'e+')
+            rmses.append(rmse_val)
+            r2s.append(r2_val)
         
-        mse = ' & '.join(mses)
-        r2 = ' & '.join(r2s)
+        # & \mbox{rmse} & \mbox{rmse}
+        rmse = '} & \\mbox{'.join(rmses)
+        r2 = '} & \\mbox{'.join(r2s)
+        rmse = '\\mbox{' + rmse + '}'
+        r2 = '\\mbox{' + r2 + '}'
         
-        mse_data += '''{model} & {mse} \\\\
+        mse_data += '''{model} & {rmse} \\\\
         \hline
-        '''.format(model=models[i], mse=mse)
+        '''.format(model=models[i], rmse=rmse)
         
         r2_data += '''{model} & {r2} \\\\
         \hline
         '''.format(model=models[i], r2=r2)
 
-    iterations = len(mses)
-    columns = '*{' + str(iterations) + '}{p{0.08cm}}'
+    iterations = len(rmses)
+    columns = '*{' + str(iterations) + '}{p{1.08cm}}'
     labels = ''
     for i in range(1, iterations+1):
         labels += '& \\textbf{{{i}}}'.format(i=i)
 
-    mse_table = base.format(data=mse_data, dataset=dataset, columns=columns, labels=labels)
-    r2_table = base.format(data=r2_data, dataset=dataset, columns=columns, labels=labels)
+    rmse_table = base.format(data=mse_data, dataset=dataset, columns=columns, labels=labels, metric='RMSE')
+    r2_table = base.format(data=r2_data, dataset=dataset, columns=columns, labels=labels, metric='R2')
     
-    print('MSE TABLE')
+    print('rmse TABLE')
     print('\n\n')
     
-    print(mse_table)
+    print(rmse_table)
     
     print('R2 TABLE')
     print('\n\n')
--- src/main_doc.py
+++ src/main_doc.py
@@ -5,10 +5,15 @@ from learning.misc.doc import *
 if __name__ == '__main__':
     
     # generate_states_figure()
-    generate_grid_figure()
+    # generate_grid_figure()
     # generate_moore_figure()
     # generate_interval_figure()
 
+    # generate_scores_table(dataset=DATASET8_DENSITY)
     # generate_score_by_iteration_table(dataset=DATASET8_DENSITY)
+    # generate_scores_table(dataset=DATASET3_DENSITY)
     # generate_score_by_iteration_table(dataset=DATASET3_DENSITY)
+    # generate_scores_table(dataset=DATASET9_DENSITY)
     # generate_score_by_iteration_table(dataset=DATASET9_DENSITY)
+    # generate_scores_table(dataset=DATASET10_DENSITY)
+    # generate_score_by_iteration_table(dataset=DATASET10_DENSITY)
